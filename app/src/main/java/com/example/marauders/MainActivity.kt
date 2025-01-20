4ed397272313b64223f2bc98abdabc210f1188c9
somsak <somsakk@tisco.co.th>
2025-01-20 11:52:54 +0700

    Test push.

--- app/src/main/java/com/example/marauders/MainActivity.kt
+++ app/src/main/java/com/example/marauders/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MaraudersTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Androidd",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
